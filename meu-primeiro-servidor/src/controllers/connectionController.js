0b5eb2d3224f47a5c514910a908129709bead917
Leticia Desiderio <letidesiderio18@gmail.com>
2021-10-19 18:13:10 -0300

    atualizar atividade completa com mvc

--- meu-primeiro-servidor/src/controllers/connectionController.js
+++ meu-primeiro-servidor/src/controllers/connectionController.js
@@ -1,13 +1,39 @@
 const connectionJson = require("../models/connection.json");
 
 const searchMessage = (request, response) => {
-  const idRequest = request.params.id;
-  let idFound = connectionJson.find((msg) => msg.id == idRequest);
-  response.status(200).send(idFound);
+  try {
+    const idRequest = request.params.id;
+
+    let idFound = connectionJson.find((msg) => msg.id == idRequest);
+
+    if (idFound == undefined) {
+      response.status(404).json([
+        {
+          message: "Este id não foi encontrado.",
+        },
+      ]);
+    }
+
+    response.status(200).send(idFound);
+  } catch (error) {
+    response.status(400).json([
+      {
+        message: "Error.",
+      },
+    ]);
+  }
 };
 
 const allMessage = (request, response) => {
-  response.status(200).send(connectionJson);
+  try {
+    response.status(200).send(connectionJson);
+  } catch (error) {
+    response.status(404).json([
+      {
+        message: "Não foi possível encontrar as mensagens.",
+      },
+    ]);
+  }
 };
 
 module.exports = {
--- servidor-em-aula/src/controllers/filmesController.js
+++ servidor-em-aula/src/controllers/filmesController.js
@@ -1,47 +1,113 @@
 const moviesJson = require("../models/ghibli.json");
 
 const getMessage = (request, response) => {
-  response.status(200).send([
-    {
-      message: "API de filmes Ghibli da turma On14.",
-    },
-  ]);
+  try {
+    response.status(200).send([
+      {
+        message: "API de filmes Ghibli da turma On14.",
+      },
+    ]);
+  } catch (error) {
+    response.status(404).json([
+      {
+        message: "Não foi possível encontrar esta mensagem.",
+      },
+    ]);
+  }
 };
+
 const getAll = (request, response) => {
-  response.status(200).send(moviesJson);
+  try {
+    response.status(200).send(moviesJson);
+  } catch (error) {
+    response.status(404).json([
+      {
+        message: "Não foi possível encontrar estes filmes.",
+      },
+    ]);
+  }
 };
 
 const getTitle = (request, response) => {
-  let titleRequest = request.query.title.toLocaleLowerCase();
-  let titleFound = moviesJson.filter((movies) =>
-    movies.title.toLocaleLowerCase().includes(titleRequest)
-  );
-  response.status(200).send(titleFound);
+  try {
+    let titleRequest = request.query.title.toLocaleLowerCase();
+    let titleFound = moviesJson.find((movies) =>
+      movies.title.toLocaleLowerCase().includes(titleRequest)
+    );
+
+    if (titleFound == undefined) {
+      response.status(400).json([
+        {
+          message: "Não foi possível encontrar este título.",
+        },
+      ]);
+    }
+    response.status(200).json(titleFound);
+  } catch (error) {
+    response.status(400).json([
+      {
+        message: "Error, verifique e tente novamente.",
+      },
+    ]);
+  }
 };
 
 const createMovies = (request, response) => {
-  let titleRequest = request.body.title;
-  let descriptionRequest = request.body.description;
+  try {
+    let titleRequest = request.body.title;
+    let descriptionRequest = request.body.description;
 
-  let newMovie = {
-    id: moviesJson.length + 1,
-    title: titleRequest,
-    description: descriptionRequest,
-  };
-  moviesJson.push(newMovie);
+    let newMovie = {
+      id: moviesJson.length + 1,
+      title: titleRequest,
+      description: descriptionRequest,
+    };
 
-  response.status(201).json([
-    {
-      message: "Filme cadastrado com sucesso!",
-      newMovie,
-    },
-  ]);
+
+    if (newMovie.title == "" || newMovie.description == "") {
+      response.status(406).json([
+        {
+          message: "Por favor, verifique e insira as informações corretamente.",
+        },
+      ]);
+    } else {
+      moviesJson.push(newMovie);
+    }
+    response.status(201).json([
+      {
+        message: "Filme cadastrado com sucesso!",
+        newMovie,
+      },
+    ]);
+  } catch (error) {
+    response.status(400).json([
+      {
+        message: "Error, verifique e tente novamente.",
+      },
+    ]);
+  }
 };
 
 const getById = (request, response) => {
-  const idRequest = request.params.id;
-  let moviesFound = moviesJson.find((movies) => movies.id == idRequest);
-  response.status(200).send(moviesFound);
+  try {
+    const idRequest = request.params.id;
+    let moviesFound = moviesJson.find((movies) => movies.id == idRequest);
+
+    if (moviesFound == undefined) {
+      response.status(404).json([
+        {
+          message: "Não possível encontrar este id.",
+        },
+      ]);
+    }
+    response.status(200).send(moviesFound);
+  } catch (error) {
+    response.status(400).json([
+      {
+        message: "Error, verifique e tente novamente.",
+      },
+    ]);
+  }
 };
 
 module.exports = {
